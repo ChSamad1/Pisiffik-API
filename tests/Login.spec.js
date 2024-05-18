e0b6e025308350b10736a2ec94510f6cd2374652
ChSamad1 <chs618042@gmail.com>
2024-05-18 13:13:38 -0100

    Pisiffik API

---
+++ tests/Login.spec.js
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('API Post', async({request}) => {
+    const response = await request.post('https://api.dev.pisiffik.gl/api/customer/login',
+    { 
+    data :{"phone": "+923245774610", "password":"SA@1122"},
+      headers:{"Accept":"application/json"}
+    });
+    console.log(await response.json())
+    expect(response.status()).toBe(200)
+  })
+
